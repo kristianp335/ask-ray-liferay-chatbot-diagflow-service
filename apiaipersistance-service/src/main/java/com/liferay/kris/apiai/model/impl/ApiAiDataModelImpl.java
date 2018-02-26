e4ef7b3221f0c15752efc2012a593904a4a45ddb
Kris <kris.patefield@liferay.com>
2018-02-26 18:01:33 +0000

    Increased varchar char limit on certain fields

--- apiaipersistance-service/src/main/java/com/liferay/kris/apiai/model/impl/ApiAiDataModelImpl.java
+++ apiaipersistance-service/src/main/java/com/liferay/kris/apiai/model/impl/ApiAiDataModelImpl.java
@@ -105,7 +105,7 @@ public class ApiAiDataModelImpl extends BaseModelImpl<ApiAiData>
 		TABLE_COLUMNS_MAP.put("speech", Types.VARCHAR);
 	}
 
-	public static final String TABLE_SQL_CREATE = "create table APIAI_ApiAiData (uuid_ VARCHAR(75) null,apiAiDataId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,type_ VARCHAR(75) null,result VARCHAR(75) null,action VARCHAR(75) null,fulfillment VARCHAR(75) null,speech VARCHAR(75) null)";
+	public static final String TABLE_SQL_CREATE = "create table APIAI_ApiAiData (uuid_ VARCHAR(75) null,apiAiDataId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,type_ VARCHAR(75) null,result VARCHAR(500) null,action VARCHAR(500) null,fulfillment VARCHAR(500) null,speech VARCHAR(500) null)";
 	public static final String TABLE_SQL_DROP = "drop table APIAI_ApiAiData";
 	public static final String ORDER_BY_JPQL = " ORDER BY apiAiData.apiAiDataId ASC";
 	public static final String ORDER_BY_SQL = " ORDER BY APIAI_ApiAiData.apiAiDataId ASC";
